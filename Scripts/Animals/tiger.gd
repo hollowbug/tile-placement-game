107f0dfead31a9ac8b609ea3ec1689716569dbc7
hollowbug <aitoyster@gmail.com>
2025-01-23 17:06:36 +0200

    Fixed fatal error in tiger scoring function

--- Scripts/Animals/tiger.gd
+++ Scripts/Animals/tiger.gd
@@ -9,7 +9,7 @@ func _init():
 	habitats = [Globals.TERRAIN_TYPE.JUNGLE]
 	rarity = Globals.RARITY.UNCOMMON
 	description = "<points=5> if this <this_terrain> has at least 4 tiles and no other <name_plural>"
-	
+
 func calculate_score(changes: Changes, tile: HabitatTile, _placed_tile: HabitatTile, animal_idx: int) -> void:
 		var habitat = tile.get_habitat(tile.data.terrain[animal_idx])
 		if habitat.size() < 4:
@@ -19,9 +19,8 @@ func calculate_score(changes: Changes, tile: HabitatTile, _placed_tile: HabitatT
 				continue
 			for i in range(2):
 				var animal = other_tile.preview_animals[i]
-				if other_tile.data.terrain[i] == tile.data.terrain[i] and (
+				if animal and other_tile.data.terrain[i] == tile.data.terrain[i] and (
 					animal.name == name or animal.name == "Chameleon"
 				):
 					return TileChange.new(tile, animal_idx, -tile.animal_score[animal_idx])
 		changes.add_tile(TileChange.new(tile, animal_idx, 5 - tile.animal_score[animal_idx]))
-
